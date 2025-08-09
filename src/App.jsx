9ec83075087b95051669da24ec3c2ee5a7134472
Luka Amiranashvili <luka.amiranashvili.2001@gmail.com>
2025-08-09 19:31:18 +0400

    feat: add new components to todo-app for build app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Todo from "./components/Todo";
+
 const App = () => {
-  return <div>Luka</div>;
+  return (
+    <div className="bg-stone-900 grid py-4 min-h-screen">
+      <Todo />
+    </div>
+  );
 };
 
 export default App;
